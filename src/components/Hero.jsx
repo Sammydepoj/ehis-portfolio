e6e75007cfa351eda9c8b84e5a7ed888f03725a3
sanuel <samueladepoju86@yahoo.com>
2023-10-16 22:52:44 +0100

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
       id="home"
       // data-aos="fade-up"
     >
-      <div className="absolute inset-0 top-[2rem] 3xl:top-[120px] xl:mt-36 px-5 lg:px-0 max-w-7xl justify-between mx-auto flex flex-wrap items-start gap-5">
-        <section className="text-[2rem] lg:text-[3.6rem] font-bold text-[#f5f5f5] font-[poppins-extrabold]">
+      <div className="absolute inset-0 top-[2rem] 3xl:top-[120px] xl:mt-36 px-5 lg:px-0 max-w-7xl justify-between mx-auto flex flex-wrap items-center gap-5 mt-12 md:mt-16">
+        <section className="text-[1.5rem] lg:text-[3.6rem] font-bold text-[#f5f5f5] font-[poppins-extrabold] ">
           Hi 👋, <br /> My name is <br />
           <span className="name">IRIBHOGBE, EHIS JUDE</span>
           <br />
@@ -19,7 +19,7 @@ const Hero = () => {
           <img
             src={ProfileImage}
             alt=""
-            className="rounded-full w-[17rem] h-[17rem] md:w-[27rem] md:h-[27rem] p-3 profile-image md:float-right object-cover"
+            className="rounded-full w-[17rem] h-[17rem] md:w-[27rem] md:h-[27rem] p-3 profile-image md:float-right object-cover mx-auto"
           />
         </section>
       </div>
